6283b136be2b7fb0f579d077f0855c4420575a44
zsline <sergei.dereka@gmal.com>
2021-09-03 16:00:50 +0300

    added slider and creating css

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,35 @@
 $(function () {
+    $('.feed__slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        swipe: false,
+        responsive: [
+            {
+                breakpoint: 1824,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
 
 })
