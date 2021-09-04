cdf1d3822e573a616e9b61eb2c2c858299088116
zsline <sergei.dereka@gmal.com>
2021-09-04 15:54:23 +0300

    added sliders

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,8 @@ $(function () {
         slidesToShow: 2,
         slidesToScroll: 1,
         swipe: false,
+        nextArrow: '<img class="next-arrow arrows" src="../images/arrow-right.svg" alt="">',
+        prevArrow: '<img class="prev-arrow arrows" src="../images/arrow-right.svg" alt="">',
         responsive: [
             {
                 breakpoint: 1824,
@@ -26,9 +28,69 @@ $(function () {
                     slidesToScroll: 1
                 }
             }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
+        ]
+    });
+
+    $('.current__slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        swipe: false,
+        nextArrow: '<img class="next-arrow arrows" src="../images/arrow-right.svg" alt="">',
+        prevArrow: '<img class="prev-arrow arrows" src="../images/arrow-right.svg" alt="">',
+        responsive: [
+            {
+                breakpoint: 1824,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    $('.popular__slider').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        swipe: false,
+        nextArrow: '<img class="next-arrow arrows" src="../images/arrow-right.svg" alt="">',
+        prevArrow: '<img class="prev-arrow arrows" src="../images/arrow-right.svg" alt="">',
+        responsive: [
+            {
+                breakpoint: 1650,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 1380,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 1230,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            }
         ]
     });
 
